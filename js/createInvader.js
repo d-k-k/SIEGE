072dca272b601e81454a956ca2927c72653a1576
Eric Wu <eric.ics211@gmail.com>
2015-07-19 18:12:04 -1000

    Issue #1: Updated basic invader move AI

--- js/createInvader.js
+++ js/createInvader.js
@@ -8,7 +8,32 @@
  *  AI for moving invaders.
  */
 function invaderMoveAI() {
-    
+    var maxY = 0;
+    var minY = 0;
+    var alienHeight = alien[0].height;
+    for (var i = 0; i < aliens.length; i++) {
+        var alien = aliens[i];
+        if (alien.isAlive === false) {
+            continue;
+        }
+        alien.y += alien.height * alien.moveDirection;
+        maxY = Math.max(maxY, alien.y);
+        minY = Math.min(minY, alien.y);
+    }
+    if (maxY > (game.height - 10 - alienHeight/2) || minY < (10 + alienHeight/2) {
+        for (var i = 0; i < aliens.length; i++) {
+            if (alien.isAlive === false) {
+                continue;
+            }
+            aliens[i].y += aliens[i].height * (aliens[i].direction * -1);
+            if (aliens[i].x > game.width/2) {
+                aliens[i].x += aliens[i].width;
+            }
+            if (aliens[i].x < game.width/2) {
+                aliens[i].x -= aliens[i].width;
+            }
+        }
+    }
 }
 
 
