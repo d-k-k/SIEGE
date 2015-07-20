d480191003f89c95cc464b1dac1a282c6328794a
chocowzzrd <samuel_h_park@hotmail.com>
2015-07-19 15:17:10 -1000

    Issue #2: Updated file

--- js/createEntity.js
+++ js/createEntity.js
@@ -6,62 +6,47 @@ Don't call this directly.
 function createEntity() {
 	var ent = {};
 
-	ent.x = -100;
-	ent.y = -100;
-	ent.width = -1;
-	ent.height = -1;
-	ent.speed = -1; //You need to modify this.
+	ent.x = -100;//the default x ordinance of entity
+	ent.y = -100;//the defualt y ordinance of entity
+	ent.width = -1;//How wide the entity is
+	ent.height = -1;//How tall the entity is 
+	ent.speed = -1;//movement speed
 	ent.moveDirection = 'none';
+	ent.hp = 1;//For now everything will have 1 hp
+	ent.type = "entity";//
+	isAlive = true;//Are you still alive bro?
 
 	ent.moveUpdate = function () { console.log("Error entity move accessed."); };
 
-	ent.getHitBox = function () {
-		var rect = {};
+	ent.getHitBox = function () {//based off the center point of the sprite image. Hit box should be around the sprite, not the sprite itself
+		var rect = {};//create hit box object
 
-		rect.x = this.x;
+		rect.x = this.x;//copy over dimensions and position
 		rect.y = this.y;
 		rect.width = this.width;
 		rect.height = this.height;
 
-		return rect;
+		return rect;//return the hitbox object
 	}; //end getHitBox
 
-
+	ent.Damage = function (amountOfDamageBeingTaken) {
+		var currentHp = ent.hp;//gets current hp
+		currentHP = currentHp - amountOfDamageBeingTaken;//calculates damage
+		if (currentHp == 0 || currentHp < 0) {//hp check
+			ent.isAlive = false;//sets isAlive to false
+			ent.Death();//call to the Death function 
+		}
+	};
+	
+	ent.Death = function () {
+		ent.isAlive = false;//set isAlive to false
+		ent.speed = 0;//set speed to 0
+		ent.direction = "none";//set direction to none
+		//remove sprite code here
+	};
+	
+	ent.spawnAt = function () {console.log("Error entity spawnAt accessed."); };//takes parameters of where you want to spawn entity	
 
 	return ent; //DONT FORGET THIS
 
 } //end createEntity
-
-/*---------------------------------------------------------------------------------------------------------
-Creates a player game object
-*/
-function createPlayer() {
-	var ent = createEntity();
-
-	ent.intervalShoot = 500; //milliseconds
-	ent.counterShoot = 0;
-	ent.speed = 3;
-
-	ent.moveUpdate = function() {
-		switch (this.moveDirection) {
-
-			case 'up':
-				this.y -= speed;
-			break;
-			case 'down':
-				this.y += speed;
-			break;
-			case 'none':
-			break;
-			default:
-				console.log('Error unknown direction');
-			break;
-		} //end switch moveDirection
-
-
-		//need to update visuals.
-
-
-	} //end moveUpdate
-} //end createPlayer
-
