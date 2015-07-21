d2633c80e5bc618b35ca13f1ebce2608ce704a60
Nick Izawa <izawan@hawaii.edu>
2015-07-20 14:57:37 -1000

    Code Update
    Moved some stuff around. Hit effects moved to collisionEffects from playerUpdates, obviously collisionEffects should hold the EFFECTS OF THE COLLISION. Added a boss hit detection (this applies to boss parts as well). Sort of added collision detection, still kind of unsure on how to go about it, will create an issue.

--- js/logicGame.js
+++ js/logicGame.js
@@ -1,10 +1,10 @@
 function logicGame() 
 {
-  player1Update();
-	player2Update();
-	invadersUpdate();
-	onscreenVisuals();
-	collisionEffects();
+    player1Update();
+    player2Update();
+    invadersUpdate();
+    onscreenVisuals();
+    collisionEffects();
 } //end logicGame
 
 function player1Update()
@@ -29,18 +29,10 @@ function player1Update()
         player1.isShooting == false;
     }
         
-    /* Hit checks */
-    if(player1.isHit)
+    /* Collision Detection */
+    if(player1.getHitBox() /* .overlaps?? */ )
     {
-        if(player1.hasShield)
-        {
-            player1.hasShield = false;
-        }
-        else 
-        {
-            player1.death();
-            gameState = gsResult;
-        }
+        collisionEffects(player1, someObject);
     }
 }
 
@@ -66,18 +58,10 @@ function player2Update()
         player2.isShooting == false;
     }
         
-    /* Hit checks */
-    if(player2.isHit)
+    /* Collision Detection */
+    if(player1.getHitBox() /* .overlaps?? */ )
     {
-        if(player2.hasShield)
-        {
-            player2.hasShield = false;
-        }
-        else 
-        {
-            player2.death();
-            gameState = gsResult;
-        }
+        collisionEffects(player1, someObject);
     }
 }
 
@@ -98,35 +82,60 @@ function collisionEffects(object1, object2)
     /* Checks if player is hit by any bullet or invader */
     if(object1.type == "player" && (object2.type == "invaderBullet" || object2.type == "invader" || object2.type == "playerBullet"))
     {
-        object1.isHit = true;
-        //explosions and stuff
+        if(player1.hasShield)
+        {
+            player1.hasShield = false;
+        }
+        else 
+        {
+            player1.death();
+            gameState = gsResult;
+        }
+        //TODO: Death animation
     }
     else if((object1.type == "invaderBullet" || object1.type == "invader" || object1.type == "playerBullet") && object2.type == "player")
     {
-        object2.isHit = true;
-        //explosions and stuff
+        if(object1.hasShield)
+        {
+            object1.hasShield = false;
+        }
+        else 
+        {
+            object1.death();
+            gameState = gsResult;
+        }
+        //TODO: Death animation
     }
     
     /* Checks player bullets with invaders */
     else if(object1.type == "invader" && object2.type == "playerBullet") 
     {
-        object1.isHit = true;
-        //explosions and stuff
+        object1.death();
+        //TODO: Death animation
     }
     else if(object1.type == "playerBullet" && object2.type == "invader")
     {
-        object2.isHit = true;
-        //explosions and stuff
+        object2.death();
+        //TODO: Death animation
     }
     
-    /* Checks player bullets with other players */
-    else if(object1.type == "player" && object2.type == "playerBullet")
+    /* Checks for boss */
+    else if(object1.type == boss && object2.type == playerBullet)
     {
-        object1.isHit = true;
-        //explosions and stuff
+        object1.damage();
+        if(object1.health == 0)
+        {
+            object1.death(); //this could be in context of boss parts too
+            //TODO: Death animation
+        }
     }
-    else if(object1.type == "playerBullet" && object2.type == "player")
+    else if(object1.type == playerBullet && object2.type == boss)
     {
-        object2.isHit = true;
-        //explosions and stuff
+        object2.damage();
+        if(object2.health == 0)
+        {
+            object2.death(); //see above
+            //TODO: Death animation
+        }
     }
+}
