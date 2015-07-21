15444b3605a4f39509b57aafbc5706e4ca4362ff
chocowzzrd <samuel_h_park@hotmail.com>
2015-07-20 16:45:39 -1000

    Issue #4: Updated invaders

--- js/createInvader.js
+++ js/createInvader.js
@@ -1,7 +1,88 @@
 
+function createInvader(invadeType) {
+	var ent = {};
 
+	ent.x = -100;//the default x ordinance of entity
+	ent.y = -100;//the default y ordinance of entity
+	ent.width = -1;//Based on invader image given, to be changed later	
+	ent.height = -1;//based on invader image given, to be changed later
+	ent.speed = 5;//TBD: testing required?
+	ent.moveDirection = 'none';//up down left right
+	ent.hp = 1;//For now everything will have 1 hp
+	ent.type = "invader";//
+	isAlive = true;//invader is alive
+	
+	ent.invaderType = invadeType;//1 being normal, or 2 being suicide
+	ent.collisionDamage = 1;//amount of damage dealt of collision
+	ent.intervalShoot = 500;//minimum time between shots in ms
+	ent.counterShoot = 0;//?
+	ent.allBullets = [{}, {}, {}]; //?
+	ent.maxShootsBullets = 3;//max number of bullets an invader has
 
+	
+	ent.moveUpdate = function () {//calls the invaderMoveAI or suicideInvaderAI to handle movement
+		if(ent.invaderType == 1) {
+			call invaderMoveAI.call(ent);
+		}
+		else if (ent.invaderType == 2) {
+			call suicideInvaderAI.call(ent);
+		}
+	};
+	
+	ent.shoot = function () {//calls the shootingAI function to handle shots 
+		call shootingAI.call(ent);
+	};
 
+	ent.getHitBox = function () {//based off the center point of the sprite image. Hit box should be around the sprite, not the sprite itself
+		var rect = {};//create hit box object
+
+		rect.x = this.x;//copy over dimensions and position
+		rect.y = this.y;
+		rect.width = this.width;
+		rect.height = this.height;
+
+		return rect;//return the hitbox object
+	}; //end getHitBox
+
+	ent.damage = function (amountOfDamageBeingTaken) {
+		var currentHp = ent.hp;//gets current hp
+		currentHp = currentHp - amountOfDamageBeingTaken;//calculates damage
+		if (currentHp == 0 || currentHp < 0) {//hp check
+			ent.isAlive = false;//sets isAlive to false
+			ent.death();//call to the Death function 
+		}
+	};
+	
+	ent.death = function () {
+		ent.isAlive = false;//set isAlive to false
+		ent.speed = 0;//set speed to 0
+		ent.direction = "none";//set direction to none
+		if(ent.invaderType == 1) {//if the invader that was killed was a normal invader
+			if(Math.random() < .15) {//and if it hits the 15% chance of spawning a suicide invader
+				//spawn a suicide invader at its location
+			}
+		}
+		//remove sprite code here
+	};
+	
+	ent.spawnAt = function (centerXvalue, centerYvalue) {//spawns invaders at location
+		ent.x = centerXvalue;
+		ent.y = centerYvalue;
+	};
+	
+	ent.dealCollisionDamage = function () {//currently the same as damage. (for future explosions)
+		var currentHp = ent.hp;//gets current hp
+		currentHp = currentHp - collisionDamage;//calculates damage
+		if (currentHp == 0 || currentHp < 0) {//hp check
+			ent.isAlive = false;//sets isAlive to false
+			ent.death();//call to the Death function 
+		}
+	};
+
+	
+	return ent; //DONT FORGET THIS
+
+}
 
 
 /**
@@ -18,6 +99,7 @@ function invaderMoveAI() {
 function suicideInvaderAI() {
 
 }
+
 
 /**
  *  AI for controlling how the invaders will shoot.
