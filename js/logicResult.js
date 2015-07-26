8e0c29493409a1f959c0480f26d5cc1df07d2688
Nick Izawa <izawan@hawaii.edu>
2015-07-26 01:17:49 -1000

    Issue #14: Added State Changer and Docs
    Added a function to menu mode from the result screen. Also added a check to see if name is entered. See issue comments for more info on implementation, might end up not including the check, doing everything in inputResult.

--- js/logicResult.js
+++ js/logicResult.js
@@ -1,5 +1,17 @@
+/*---------------------------------------------------------
+    logicResult()
+*/
 
-
+/* Checks for conditions to switch back to the menu */
 function logicResult() {
-
+    /* Check if name is entered, if so switch to menu */
+    if(nameEntered) { 
+        prepandSwitchToMenu();
+    }
 } //end logicResult
+
+/* Changes the game state and sets up the visuals for the menu */
+function prepandSwitchToMenu() {
+        gameState = gsMenu;
+        setupMenuVisuals();
+}
