f30d35cbe3c70dbbb3adb8de4aef352e3c256f26
Nick Izawa <izawan@hawaii.edu>
2015-07-26 01:14:32 -1000

    Issue #10: Added State Switcher and Docs
    Added function to switch to game mode from the menu, also added documentation.

--- js/logicMenu.js
+++ js/logicMenu.js
@@ -1,5 +1,16 @@
+/*---------------------------------------------------------
+    logicMenu()
+        
+    prepandSwitchToGame()
+*/
 
-
+/* Does...nothing? */
 function logicMenu() {
 
 } //end logicMenu
+
+/* Changes the game state and sets up the game visuals */
+function prepandSwitchToGame() {
+        gameState = gsGame;
+        setupGameVisuals();
+}
