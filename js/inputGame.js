103822114d879d28dd434907d3ddf587f1b0267f
chocowzzrd <samuel_h_park@hotmail.com>
2015-07-27 13:05:53 -1000

    Updated input for Menu and Game

--- js/inputGame.js
+++ js/inputGame.js
@@ -9,6 +9,7 @@ function inputGame() {
 
 function inputGameKeyboardBackup() {
 
+	/*
 	//w
 	if(keyboardKeys[87] === 'down') { allPlayers[0].moveDirection = 'up'; }
 	//s
@@ -20,5 +21,49 @@ function inputGameKeyboardBackup() {
 	//k
 	else if(keyboardKeys[75] === 'down') { allPlayers[1].moveDirection = 'down'; }
 	else { allPlayers[1].moveDirection = 'none';}
+	*/
+	
+	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	//var gamepads = navigator.getGamepads();
+	if (!gamepads)
+		return;
+	var gp = gamepads[0];
+	/*if (gp.axes[1] > 0.9) {//down
+		allPlayers[0].moveDirection = 'down';
+	} else if(gp.axes[1] < -0.9) {//up
+		allPlayers[0].moveDirection = 'up';
+	}
+	else { allPlayers[0].moveDirection = 'none';}*/
+	if (gp.axes[1] > .8) {//down
+		allPlayers[0].moveDirection = 'down';
+	} else if(gp.axes[1] < -.8) {//up
+		allPlayers[0].moveDirection = 'up';
+	}
+	else { allPlayers[0].moveDirection = 'none';}
+	
+	if(gp.axes[3] > 0.8) {//right
+		allPlayers[1].moveDirection = 'down';
+	} else if(gp.axes[3] < -0.8) {//up
+		allPlayers[1].moveDirection = 'up';
+	}
+	else { allPlayers[1].moveDirection = 'none';}
+	
+	
+	//shooting
+	if(buttonPressed(gp.buttons[0])) {
+		allPlayers[0].shoot();
+	}
+	if(buttonPressed(gp.buttons[3])) {
+		allPlayers[1].shoot();
+	}
+	
+	
 
 } //end inputGameKeyboardBackup
+
+function buttonPressed(b) {
+  if (typeof(b) == "object") {
+    return b.pressed;
+  }
+  return b == 1.0;
+}
--- js/inputMenu.js
+++ js/inputMenu.js
@@ -3,11 +3,48 @@
 function inputMenu() {
 
 
-	//if enter key
+	/*if enter key
+	var gamepads = navigator.getGamepads();//gets all the gamepads
+	
+	for (var i = 0; i < gamepads.length; ++i)//loop through each gamepad
+    {
+		if(gamepads.length > 2) {//if there are more than 2 gamepads, break
+			break;
+		}
+		
+        var currentGamepad = gamepads[i];
+		
+        if (currentGamepad.buttons[0] === 1) {
+			prepAndSwitchToGame();
+		}
+    }
 	if( keyboardKeys[13] === 'down' ) {
 		prepAndSwitchToGame();
+	}*/
+	
+	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	//var gamepads = navigator.getGamepads();
+	if (gamepads[0] == null) {
+		return;
 	}
-
+	/*if (!gamepads || gamepads.length() > 2) {
+		return;
+	}*/
+	var gp = gamepads[0];
+	if (buttonPressed(gp.buttons[0])) {//down
+		prepAndSwitchToGame();
+	}
+	/*if( keyboardKeys[13] === 'down' ) {
+		prepAndSwitchToGame();
+	}*/
+	
 
 
 } //end inputMenu
+
+function buttonPressed(b) {
+  if (typeof(b) == "object") {
+    return b.pressed;
+  }
+  return b == 1.0;
+}
