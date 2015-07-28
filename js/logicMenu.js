ac24ec1003fd0e451baff1ae2f707b3a1ec109da
Nick Izawa <izawan@hawaii.edu>
2015-07-27 14:41:56 -1000

    Issue #10: Updated prepandSwitchToGame
    Added spawns for players/invaders and kills all bullets. Updated visual call.

--- js/logicMenu.js
+++ js/logicMenu.js
@@ -4,13 +4,26 @@
     prepandSwitchToGame()
 */
 
-/* Does...nothing? */
+/* Everything done in input. */
 function logicMenu() {
 
 } //end logicMenu
 
-/* Changes the game state and sets up the game visuals */
+/* Sets up the game */
 function prepandSwitchToGame() {
-        gameState = gsGame;
-        setupGameVisuals();
+    /* Spawns all invaders and players.
+    for(int i = 0; i < allInvaders.length; i++) {
+        //allInvaders[i].spawn();
+    }
+    for(int j = 0; j < allPlayers.length; j++) {
+        allPlayers[j].spawn();
+    } */
+    
+    /* Kills all bullets */
+    for(int k = 0; k < allEntities.length; k++) {
+        if(allEntities[k].type == "bullet") {
+            allEntities[k].isAlive = false;
+        }
+    }
+    placeScreenVisuals(allGameVisuals);
 }
