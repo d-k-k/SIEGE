4d4f87d779ea38eb48f410f859a63d66d7ff4ba0
Nick Izawa <izawan@hawaii.edu>
2015-07-27 15:37:19 -1000

    Issue #10: Added menu switcher
    Added prepandSwitchToMenu().

--- js/logicMenu.js
+++ js/logicMenu.js
@@ -1,29 +1,20 @@
 /*---------------------------------------------------------
+
     logicMenu()
-        
-    prepandSwitchToGame()
+
+    prepandSwitchToMenu()
+
 */
 
-/* Everything done in input. */
 function logicMenu() {
-
+	
 } //end logicMenu
 
-/* Sets up the game */
-function prepandSwitchToGame() {
-    /* Spawns all invaders and players.
-    for(int i = 0; i < allInvaders.length; i++) {
-        //allInvaders[i].spawn();
-    }
-    for(int j = 0; j < allPlayers.length; j++) {
-        allPlayers[j].spawn();
-    } */
+/* Draws visuals for the menu and changes gamestate */
+function prepandSwitchToMenu() {
     
-    /* Kills all bullets */
-    for(int k = 0; k < allEntities.length; k++) {
-        if(allEntities[k].type == "bullet") {
-            allEntities[k].isAlive = false;
-        }
-    }
-    placeScreenVisuals(allGameVisuals);
-}
+    placeScreenVisuals( allMenuVisuals );
+
+    gameState = gsMenu;
+    
+} //end prepandSwitchToMenu
