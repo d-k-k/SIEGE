b6efcdede71e94adad1436fc525860e2c4502748
Nick Izawa <izawan@hawaii.edu>
2015-07-27 15:38:15 -1000

    Bug Fix
    Used var keyword instead of int....

--- js/logicResult.js
+++ js/logicResult.js
@@ -6,11 +6,11 @@
 
 /* Updates leaderboard */
 function logicResult() {
-    for(int i = 0; i < allPlayers.length; i++) {
-        for(int j = 0; j < leaderBoard.length; j++) {
+    for(var i = 0; i < allPlayers.length; i++) {
+        for(var j = 0; j < leaderBoard.length; j++) {
             if(allPlayers[i].score >= leaderBoard[j].score) {
                 /* Shift all leaderBoard scores and insert new score */
-                for(int k = j; k < leaderBoard.length; k++) {
+                for(var k = j; k < leaderBoard.length; k++) {
                     leaderBoard[k + 1] = leaderBoard[k];
                 }
                 leaderBoard[j].score = allPlayers[i].score;
@@ -21,5 +21,7 @@ function logicResult() {
 
 /* Preps the game */
 function prepandSwitchToMenu() {
-    placeScreenVisuals(allMenuVisuals);
-}
+    
+    placeScreenVisuals( allMenuVisuals );
+    
+} //end prepandSwitchToResult
